7ca9975987a80f81ea5bac32991ef1ac4f5ed0b2
cgmaterial <contact.cgmaterial@gmail.com>
2025-01-24 10:20:01 +0530

    header update

--- test/utils.py
+++ test/utils.py
@@ -10,7 +10,7 @@ from PIL import Image
 payload = ""
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
     "Accept": "application/json, text/plain, */*",
     "Accept-Language": "en",
     "Accept-Encoding": "gzip, deflate, br, zstd",
