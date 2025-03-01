9915dfc2178ae1b5a777953fea44b5891d5d714a
cgmaterial <contact.cgmaterial@gmail.com>
2025-03-01 12:05:33 +0530

    fixes

--- tools/utils.py
+++ tools/utils.py
@@ -8,9 +8,9 @@ import platform
 
 
 if platform.system() == 'Windows':
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0"
 else:
-    user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0"
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0"
 
 headers = {
     "Accept": "application/json, text/plain, */*",
